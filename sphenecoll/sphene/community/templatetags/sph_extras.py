ffc3f4c450163e4ee3aef83a19e6e76ba2f1ad49
Herbert <herbert@poul.at>
2007-02-04 15:05:12 +0000

    added news macro for wiki.

--- sphenecoll/sphene/community/templatetags/sph_extras.py
+++ sphenecoll/sphene/community/templatetags/sph_extras.py
@@ -1,7 +1,9 @@
 from django import template
 from time import strftime
+import re
 
 from sphene.sphwiki.models import WikiAttachment
+from sphene.sphboard.models import Category, Post
 
 register = template.Library()
 
@@ -19,7 +21,45 @@ class ImageMacro (object):
                 if params.has_key( paramName ):
                     el.setAttribute( paramName, params[paramName] )
             return el
-        return doc.createTextNode("<b>Error, no 'id' given for img macro.</b>")
+        return doc.createTextNode("Error, no 'id' given for img macro.")
+
+class HTML:
+    type = "text"
+    attrRegExp = re.compile(r'\{@([^\}]*)=([^\}]*)}') # {@id=123}
+    
+    def __init__(self, value):
+        self.value = value
+
+    def attributeCallback(self, match) :
+        self.parent.setAttribute(match.group(1), match.group(2))
+        
+    def handleAttributes(self) :
+        self.value = self.attrRegExp.sub(self.attributeCallback, self.value)
+
+    def toxml(self):
+        return self.value
+
+"""
+displays threads in the given board category
+"""
+class NewsMacro (object):
+   def handleMacroCall(self, doc, params):
+       if not params.has_key( 'category' ):
+           return doc.createTextNode("Error, no 'category' or 'template' given for news macro.")
+
+       templateName = 'wiki/news.html'
+       if params.has_key( 'template' ): templateName = params['template']
+
+       threads = Post.objects.filter( category__id = params['category'], thread__isnull = True )
+
+       t = template.loader.get_template( templateName )
+       baseURL = ''
+       if params.has_key( 'baseURL' ): baseURL = params['baseURL']
+       c = template.Context({ 'threads': threads,
+                              'baseURL': baseURL,
+                              })
+       
+       return HTML( t.render(c) )
 
 @register.filter
 def sph_markdown(value, arg=''):
@@ -32,12 +72,13 @@ def sph_markdown(value, arg=''):
     else:
         save_mode = arg == 'safe'
         md = markdown.Markdown(value,
-                                 extensions = [ 'footnotes', 'wikilink', 'macros' ],
-                                 extension_configs = { 'wikilink': [ ( 'base_url', '../' ),
-                                                                    ],
-                                                       'macros': [ ( 'macros',
-                                                                     { 'helloWorld': SimpleHelloWorldMacro(),
-                                                                       'img': ImageMacro(), } )]},
+                               extensions = [ 'footnotes', 'wikilink', 'macros' ],
+                               extension_configs = { 'wikilink': [ ( 'base_url', '../' ),
+                                                                   ],
+                                                     'macros': [ ( 'macros',
+                                                                   { 'helloWorld': SimpleHelloWorldMacro(),
+                                                                     'img': ImageMacro(),
+                                                                     'news': NewsMacro(), } )]},
                                  )
         return md.toString()
 
