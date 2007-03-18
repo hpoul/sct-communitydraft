c2d8cec832a67f4afc07275d989dcb0223daa2a6
Herbert <herbert@poul.at>
2007-03-18 19:30:05 +0000

    Removed leaguesystem and polls.. since they were just dummy applications.

--- goimcommunity/urls.py
+++ goimcommunity/urls.py
@@ -20,14 +20,8 @@ urlpatterns = patterns('',
     # Uncomment this for admin:
                        (r'^admin/', include('django.contrib.admin.urls')),
 
-                       (r'^polls/', include('goimcommunity.polls.urls')),
-		       (r'^league/', include('goimcommunity.leaguesystem.urls')),
-
                        (r'^board/', include('sphene.sphboard.urls')),
-                       (r'^rewrite/(?P<groupName>\w+)/board/', include('sphene.sphboard.urls'), {'urlPrefix': '' }),
-                       (r'^rewrite/(?P<groupName>\w+)/wiki/',  include('sphene.sphwiki.urls'), {'urlPrefix': '' }),
-		       (r'^rewrite/\w+/accounts/login/$', 'django.contrib.auth.views.login'),
-		       (r'^rewrite/\w+/accounts/logout/$', 'django.contrib.auth.views.logout' ),
+                       
                        (r'^(?P<urlPrefix>test/(?P<groupName>\w+))/board/', include('sphene.sphboard.urls')),
                        (r'^(?P<urlPrefix>test/(?P<groupName>\w+))/wiki/',  include('sphene.sphwiki.urls')),
 
