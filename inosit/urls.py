079b0061765c27b39eec08c44e35653227498bf2
Herbert <herbert@poul.at>
2007-04-08 20:55:17 +0000

    moved inosit project into another project.

--- inosit/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('',
-    # Example:
-    # (r'^inosit/', include('inosit.foo.urls')),
-
-    # Uncomment this for admin:
-#     (r'^admin/', include('django.contrib.admin.urls')),
-)
