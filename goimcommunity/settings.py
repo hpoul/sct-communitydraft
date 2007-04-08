e74bef87e962fc55b56082c004f0672ae381ec5e
Herbert <herbert@poul.at>
2007-04-08 20:36:27 +0000

    cleaned up configuration

--- goimcommunity/settings.py
+++ goimcommunity/settings.py
@@ -1,4 +1,7 @@
-# Django settings for goimcommunity project.
+# Django settings for community project.
+
+# You should not modify anything in this file,
+# but in settings_local.py
 
 import os
 ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
@@ -7,12 +10,15 @@ from django.conf.urls.defaults import *
 DEBUG = True
 TEMPLATE_DEBUG = True
 
+# The following settings should be customized in settings_local.py !!!
+
 ADMINS = (
     # ('Your Name', 'your_email@domain.com'),
     ('Herbert Poul', 'herbert.poul@gmail.com'),
 )
 
 MANAGERS = ADMINS
+
 
 DATABASE_ENGINE = 'postgresql'           # 'postgresql', 'mysql', 'sqlite3' or 'ado_mssql'.
 DATABASE_NAME = 'djangotest'             # Or path to database file if using sqlite3.
@@ -87,13 +93,6 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 
 ROOT_URLCONF = 'goimcommunity.urls'
 
-SPH_HOST_MIDDLEWARE_URLCONF_DEFAULT = (
-#    (r'^/board/', include('sphene.sphboard.urls'), { 'groupName': None }),
-#    (r'^/wiki/',  include('sphene.sphwiki.urls'), { 'groupName': None }),
-    (r'^/accounts/login/$', 'django.contrib.auth.views.login'),
-    (r'^/accounts/logout/$', 'django.contrib.auth.views.logout' ),
-)
-
 SPH_HOST_MIDDLEWARE_URLCONF_MAPPING = {
     'community.spacecombat2.net': { 'groupName': 'SpaceCombat2' },
     'community.spacecombat2.com': { 'groupName': 'SpaceCombat2' },
@@ -109,8 +108,9 @@ SPH_HOST_MIDDLEWARE_URLCONF_MAP = {
     'chs.sphene.net': { 'urlconf': 'urlconfs.sc2_community',
                         'params': { 'groupName': 'chs' } },
     '127.0.0.1:8000': { 'urlconf': 'urlconfs.sc2_community',
-                        'params': { 'groupName': 'Sphene' }
-                        }
+                        'params': { 'groupName': 'sphene' }
+                        },
+    r'^(?P<groupName>\w+).localhost.*$': { 'urlconf': 'urlconfs.sc2_community', },
 }
 
 TEMPLATE_DIRS = (
@@ -122,19 +122,16 @@ TEMPLATE_DIRS = (
 
     ROOT_PATH + '/../../communitytools/sphenecoll/templates',
 
-#    '/home/kahless/dev/python/diamandas/myghtyboard/templates',
-#    '/home/kahless/dev/python/diamandas/wiki/templates',
 )
 
 import sys
-#sys.path.append('/home/kahless/dev/python/diamanda/diamandas/')
-#sys.path.append('/home/kahless/dev/python/diamanda')
 sys.path.append(ROOT_PATH)
 sys.path.append(ROOT_PATH + '/../../communitytools/sphenecoll')
 sys.path.append(ROOT_PATH + '/../inosit')
 sys.path.append(ROOT_PATH + '/../../communitytools/libs/markdown')
 sys.path.append(ROOT_PATH + '/../../communitytools/libs/custom')
 sys.path.append(ROOT_PATH + '/../../communitytools/libs/common')
+
 
 
 INSTALLED_APPS = (
@@ -148,51 +145,14 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.sitemaps',
     
-    #'goimcommunity.polls',
-    #'goimcommunity.leaguesystem',
-    
     'sphene.community',
     'sphene.sphboard',
     'sphene.sphwiki',
 
     'inosit.tracker',
 
-    #'wiki',
-    #'myghtyboard',
-
 )
 
-
-# RSS Settings
-SITE_NAME = 'Diamanda Wiki !'
-SITE_DESCRIPTION = 'A Diamanda Wiki Script'
-SITE_NEWS_LINK = '/' # where links of the RSS feeds should point
-
-# Anonymous perms Settings
-ANONYMOUS_CAN_EDIT=True
-ANONYMOUS_CAN_ADD=True
-ANONYMOUS_CAN_VIEW=True
-ANONYMOUS_CAN_SET_CURENT=False
-
-# wiki config
-WIKI_USE_PDF = False # 'htmldoc' - uses htmldoc , False - no PDF generation
-# if set to nonFalse API KEY "search" will allow also "Search this site with google"
-# requires pyGoogle, uses current SITE_ID domain name !!! example.com by default, change it to yours!
-WIKI_GOOGLE_SEARCH_API = False
-
-# myghtyboard config
-ANONYMOUS_CAN_ADD_TOPIC=True
-ANONYMOUS_CAN_ADD_POST=True
-FORUMS_USE_CAPTCHA = False # should add topic/post use captcha image
-MYGHTYBOARD_THEME='CrystalClear/' # theme = folder name in diamandas/myghtyboard/templates with ending slash
-MYGHTYBOARD_LANG='english' # pointless for CrystalClear
-
-# thumb CBC
-SITE_IMAGES_DIR_PATH = '/home/kahless/dev/python/diamanda/media/images/'
-SITE_IMAGES_SRC_PATH = '/site_media/images/'
-
-# Other
-USE_BANS = False # use WIkiBans to prevent baned from add/edit actions
 
 # settings_local overwrites a few settings from here, and has to define SECRET_KEY
 from settings_local import *
--- goimcommunity/urlconfs/sc2_community.py
+++ goimcommunity/urlconfs/sc2_community.py
@@ -5,7 +5,7 @@ from sphene.sphwiki.sitemaps import WikiSnipSitemap
 from sphene.sphboard.sitemaps import ThreadsSitemap
 from sphene.sphwiki.feeds import LatestWikiChanges
 
-defaultdict = { 'groupName': None, #'SpaceCombat2',
+defaultdict = { 'groupName': None,
                 'urlPrefix': '', }
 
 sitemaps = {
@@ -21,6 +21,7 @@ urlpatterns = patterns('',
                        (r'^$', 'django.views.generic.simple.redirect_to', { 'url': '/wiki/show/Start/' }),
                        (r'^sitemap.xml$', 'django.contrib.sitemaps.views.sitemap', { 'sitemaps': sitemaps }),
                        (r'^feeds/(?P<url>.*)/$', 'django.contrib.syndication.views.feed', {'feed_dict': feeds}),
+                       (r'^community/', include('sphene.community.urls'), { }),
                        (r'^board/', include('sphene.sphboard.urls'), defaultdict),
                        (r'^wiki/',  include('sphene.sphwiki.urls'), defaultdict),
                        (r'^accounts/login/$', 'django.contrib.auth.views.login'),
